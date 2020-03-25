a72082062a57872c996297de0b54382649ba74f7
erfendy <61782609+erfendy@users.noreply.github.com>
2020-03-25 19:46:23 +0800

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.49.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.218885, 106.838324";
+$header[] = "X-Location: -2.979033, 104.77323";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
